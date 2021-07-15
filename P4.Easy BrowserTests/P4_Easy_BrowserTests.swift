f2f20e0b113a57ae7e25230ed049f504bb56e49d
partalov <partalov@hotmail.com>
2021-07-15 14:00:15 +0200

    Initial Commit

---
+++ P4.Easy BrowserTests/P4_Easy_BrowserTests.swift
@@ -0,0 +1,33 @@
+//
+//  P4_Easy_BrowserTests.swift
+//  P4.Easy BrowserTests
+//
+//  Created by Ilija Partalov on 15.7.21.
+//
+
+import XCTest
+@testable import P4_Easy_Browser
+
+class P4_Easy_BrowserTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
